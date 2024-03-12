c1907193cff1b23eaf967d62692f123e12677a91
PDogan <65000715+PDogan233@users.noreply.github.com>
2024-03-12 16:56:14 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+
+i=0
+
+while i<5:
+ print(i)
+ i+=1
+ # if i==2:
+ #     break
+else :print("end")
+
+
+
+# print("""PDogan's "python" course""")
+# print("PDogan\'s \"python\" course")
+# print(r"PDogan\'s \"python\" course")
+
